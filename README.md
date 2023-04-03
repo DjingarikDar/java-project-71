6e559dc1fb2d56efe4e34820f87bd07d41a80f09
Djandjgava Alexey <dar@it-bis.ru>
2023-04-03 18:16:51 +0200

    implement Step5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DjingarikDar/java-project-71/workflows/hexlet-check/badge.svg)](https://github.com/DjingarikDar/java-project-71/actions)
+
+### Differ Step5
+[![asciicast](https://asciinema.org/a/573657.svg)](https://asciinema.org/a/573657)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,17 +5,24 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
 
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
-        description = "Compares two configuration files and shows a difference.")
-public class App {
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0", description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format;
     @Parameters(index = "0", description = "path to first file")
-    private String filepath1;
+    private String strFilePath1;
     @Parameters(index = "1", description = "path to second file")
-    private String filepath2;
+    private String strFilePath2;
+
+    @Override
+    public Integer call() throws Exception {
+        String resultDiff = Differ.generator(strFilePath1, strFilePath2);
+        System.out.println(resultDiff);
+        return 0;
+    }
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
