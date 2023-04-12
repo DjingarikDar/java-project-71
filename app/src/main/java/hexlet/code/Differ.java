071d9bcc54f9e0f382b91317fa69338b8ca5aebe
Djandjgava Alexey <dar@it-bis.ru>
2023-04-12 16:57:59 +0200

    implement Step6

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,23 +1,21 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
 public class Differ {
     public static String generator(String filepath1, String filepath2) throws Exception {
         Path pathToFile1 = Paths.get(filepath1).toAbsolutePath().normalize();
         Path pathToFile2 = Paths.get(filepath2).toAbsolutePath().normalize();
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> mapJsonFile1 = mapper.readValue(Files.readString(pathToFile1), new TypeReference<>() {
-        });
-        Map<String, Object> mapJsonFile2 = mapper.readValue(Files.readString(pathToFile2), new TypeReference<>() {
-        });
+        Map<String, Object> mapJsonFile1 = Parser.toMap(pathToFile1);
+        Map<String, Object> mapJsonFile2 = Parser.toMap(pathToFile2);
+        if (mapJsonFile1.isEmpty() && mapJsonFile2.isEmpty()) {
+            return "{}";
+        }
         Stream<String> streamKey = Stream.concat(mapJsonFile1.keySet().stream(), mapJsonFile2.keySet().stream());
         var keys = streamKey.collect(Collectors.toSet());
         return keys.stream()
@@ -29,36 +27,21 @@ public class Differ {
     private static String strResultOfComparingMaps(String key, Map<String, Object> map1, Map<String, Object> map2) {
         StringBuilder resultStr = new StringBuilder();
         if (map1.containsKey(key) && !map2.containsKey(key)) {
-            resultStr.append("  - ")
-                    .append(key)
-                    .append(":")
-                    .append(" ")
-                    .append(map1.get(key));
+            resultStr.append("  - ").append(key).append(": ").append(map1.get(key));
         }
         if (map2.containsKey(key) && !map1.containsKey(key)) {
-            resultStr.append("  + ")
-                    .append(key)
-                    .append(": ")
-                    .append(map2.get(key));
+            resultStr.append("  + ").append(key).append(": ").append(map2.get(key));
         }
         if (map1.containsKey(key) && map2.containsKey(key)) {
             if (map1.get(key).equals(map2.get(key))) {
-                resultStr.append("    ")
-                        .append(key)
-                        .append(": ")
-                        .append(map1.get(key));
+                resultStr.append("    ").append(key).append(": ").append(map1.get(key));
             } else {
-                resultStr.append("  - ")
-                        .append(key)
-                        .append(": ")
-                        .append(map1.get(key))
-                        .append("\n")
-                        .append("  + ")
-                        .append(key)
-                        .append(": ")
-                        .append(map2.get(key));
+                resultStr.append("  - ").append(key)
+                        .append(": ").append(map1.get(key)).append("\n")
+                        .append("  + ").append(key).append(": ").append(map2.get(key));
             }
         }
         return resultStr.toString();
     }
+
 }
